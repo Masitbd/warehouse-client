a023244c5e5e8ba3ffc98c4e4d01376a0fc32bce
Murad H Sarker <sarker24@gmail.com>
2022-05-06 01:13:13 +0200

    Fix carusal

--- src/components/Carusa/Carusal.js
+++ src/components/Carusa/Carusal.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import FirstCar from '../../images/1.png';
 import { Carousel } from 'react-responsive-carousel';
 const Carusal = () => {
     return (
         <div>
            <Carousel  autoPlay infiniteLoop showThumbs={false}>
                 <div>
-                    <img className='h-[400px]'  src="https://img.freepik.com/free-photo/long-exposure-pier_1401-392.jpg?t=st=1651425825~exp=1651426425~hmac=d0f67e6a70221d317984ef00f239cc2d092e21dfe0d1df3835f3bc327efee182&w=1380" alt='' />
-                    <p className="legend">Legend 1</p>
+                    
+                  <img className='h-96 w-full object-cover object-center'  src="https://img.freepik.com/free-photo/blue-sport-sedan-parked-yard_114579-5078.jpg?size=626&ext=jpg&ga=GA1.2.914706891.1651424576" alt='' />
+                   
+                    
                 </div>
                 <div>
-                    <img className='h-[400px]' src="https://img.freepik.com/free-photo/zadorozhne-ukraine-july-28-2019-people-having-fun-summer-lake-jumping-from-tower-copy-space_259348-8993.jpg?w=1380" alt='' />
-                    <p className="legend">Legend 2</p>
-                </div>
+                    
+                    <img className='h-96 w-full object-cover object-center'  src="https://img.freepik.com/free-photo/white-coupe-sport-car-standing-road-front-view_114579-4005.jpg?size=626&ext=jpg&ga=GA1.2.914706891.1651424576" alt='' />
+                 
+                      
+                  </div>
                 <div>
-                    <img className='h-[400px]' src="https://img.freepik.com/free-photo/road-with-yellow-lines_1112-907.jpg?t=st=1651425825~exp=1651426425~hmac=86c7f95d88459a88098bf0fc990e3469f410bacfbafaa0c89307607c3cd71c4e&w=1380" alt=''/>
-                    <p className="legend">Legend 3</p>
+                    <img className='h-96 w-full object-cover object-center' src="https://img.freepik.com/free-photo/blue-jeep-parking-public-zone_114579-4042.jpg?size=626&ext=jpg&ga=GA1.2.914706891.1651424576" alt=''/>
+                   {/*  <p className="legend">Legend 3</p> */}
                 </div>
             </Carousel> 
         </div>
